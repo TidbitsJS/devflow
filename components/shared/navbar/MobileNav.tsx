d799e8ffcf692d5c2d52083ffe0b5a362054d889
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-08-23 10:31:38 +0530

    feat: mobile nav

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignedOut } from "@clerk/nextjs";
+import { SignedOut, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -13,45 +13,47 @@ import {
 } from "@/components/ui/sheet";
 
 import { sidebarLinks } from "@/constants";
+import { Button } from "@/components/ui/button";
 
 const NavContent = () => {
   const pathname = usePathname();
+  const { userId } = useAuth();
 
   return (
-    <section className='flex h-full flex-col gap-[10px] pt-16 md:px-[25px]'>
+    <section className='flex h-full flex-col gap-6 pt-16'>
       {sidebarLinks.map((item) => {
-        const isActive = pathname === item.route;
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname === item.route;
+
+        if (item.route === "/profile") item.route = `${item.route}/${userId}`;
 
         return (
           <SheetClose asChild key={item.route}>
             <Link
               href={item.route}
               key={item.label}
-              className={`paragraph-regular flex items-center gap-4 px-4 py-[14px]
-            ${isActive ? "rounded-full text-white" : "text-black"}`}
+              className={`${
+                isActive ? "primary-gradient rounded-lg text-white" : ""
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
             >
               <Image
                 src={item.imgURL}
                 alt={item.label}
-                width={18}
-                height={18}
+                width={20}
+                height={20}
               />
-              <p>{item.label}</p>
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } text-white`}
+              >
+                {item.label}
+              </p>
             </Link>
           </SheetClose>
         );
       })}
-
-      {pathname !== "/" && pathname !== "/create-question" && (
-        <SheetClose asChild>
-          <Link
-            href='/create-question'
-            className='mt-5 w-full rounded-[40px] bg-primary-500 p-3 text-center text-white'
-          >
-            Ask a Question
-          </Link>
-        </SheetClose>
-      )}
     </section>
   );
 };
@@ -64,27 +66,44 @@ const MobileNav = () => (
         width={36}
         height={36}
         alt='hamburger icon'
-        className='cursor-pointer bg-black md:hidden'
+        className='sm:hidden'
       />
     </SheetTrigger>
 
-    <SheetContent side='left' className='border-none bg-dark-200/50'>
-      <div className='flex h-full flex-col'>
+    <SheetContent side='left' className='border-none bg-dark-200'>
+      <Link href='/' className='flex items-center gap-1'>
+        <Image
+          src='/assets/images/site-logo.svg'
+          width={23}
+          height={23}
+          alt='Dev Overflow Logo'
+        />
+
+        <p className='h2-bold font-spaceGrotesk text-white'>
+          Dev<span className='text-primary-500'>Overflow</span>
+        </p>
+      </Link>
+
+      <div className='flex min-h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto'>
         <SheetClose asChild>
           <NavContent />
         </SheetClose>
 
         <SignedOut>
-          <div className='flex flex-col gap-[10px]'>
+          <div className='flex flex-col gap-3'>
             <SheetClose asChild>
-              <Link href='/sign-in' className='rounded-[20px] text-center'>
-                Log in
+              <Link href='/sign-in'>
+                <Button className='small-medium min-h-[41px] w-full rounded-lg bg-dark-400 px-4 py-3'>
+                  <span className='primary-text-gradient'>Log In</span>
+                </Button>
               </Link>
             </SheetClose>
 
             <SheetClose asChild>
-              <Link href='/sign-up' className='rounded-[20px] text-center'>
-                Sign up
+              <Link href='/sign-up'>
+                <Button className='small-medium min-h-[41px] w-full rounded-lg border border-dark-400 bg-dark-300 px-4 py-3'>
+                  Sign up
+                </Button>
               </Link>
             </SheetClose>
           </div>
