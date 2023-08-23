1e7f97a56b1ca45c10aafd08bee5f09e79fcc141
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-08-23 13:45:10 +0530

    hide edit for answer

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -17,11 +17,7 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
   const pathname = usePathname();
 
   const handleEdit = async () => {
-    if (type === "Question") {
-      router.push(`/question/edit/${JSON.parse(itemId)}`);
-    } else if (type === "Answer") {
-      // do something
-    }
+    router.push(`/question/edit/${JSON.parse(itemId)}`);
   };
 
   const handleDelete = async () => {
@@ -45,14 +41,16 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
 
   return (
     <div className='flex items-center justify-end gap-3 max-sm:w-full'>
-      <Image
-        src='/assets/icons/edit.svg'
-        alt='edit'
-        width={14}
-        height={14}
-        className='cursor-pointer object-contain'
-        onClick={handleEdit}
-      />
+      {type === "Question" && (
+        <Image
+          src='/assets/icons/edit.svg'
+          alt='edit'
+          width={14}
+          height={14}
+          className='cursor-pointer object-contain'
+          onClick={handleEdit}
+        />
+      )}
       <Image
         src='/assets/icons/trash.svg'
         alt='trash'
