3ac5efef6e832db200e855ddcdc6c9d32c907e85
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-09-03 01:47:10 +0530

    fix: deployment error

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,4 +1,6 @@
-import { cn } from "@/lib/utils"
+import React from "react";
+
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
@@ -6,10 +8,13 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-slate-900/10 dark:bg-slate-50/10", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-slate-900/10 dark:bg-slate-50/10",
+        className
+      )}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
