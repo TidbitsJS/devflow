35e1fc1d2c2fc27d30b39c22388e5dcb58712ce8
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-09-07 23:50:11 +0530

    fix: theme dropdown position

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -38,7 +38,7 @@ const Theme = () => {
           )}
         </MenubarTrigger>
 
-        <MenubarContent className='absolute -right-5 mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300'>
+        <MenubarContent className='absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300'>
           {themes.map((item) => (
             <MenubarItem
               key={item.value}
