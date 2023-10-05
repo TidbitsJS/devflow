da0870c0d8901f36f840362756b3eb2c440a151d
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-10-06 00:49:34 +0530

    fix: little light color change

--- components/form/Profile.tsx
+++ components/form/Profile.tsx
@@ -93,7 +93,7 @@ const Profile = ({ clerkId, user }: Params) => {
               </FormLabel>
               <FormControl>
                 <Input
-                  className='no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 min-h-[56px] border'
                   placeholder='Your Name'
                   {...field}
                 />
@@ -113,7 +113,7 @@ const Profile = ({ clerkId, user }: Params) => {
               </FormLabel>
               <FormControl>
                 <Input
-                  className='no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 min-h-[56px] border'
                   placeholder='Your username'
                   {...field}
                 />
@@ -134,7 +134,7 @@ const Profile = ({ clerkId, user }: Params) => {
               <FormControl>
                 <Input
                   type='url'
-                  className='no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 min-h-[56px] border'
                   placeholder='Your Portfolio link'
                   {...field}
                 />
@@ -154,7 +154,7 @@ const Profile = ({ clerkId, user }: Params) => {
               </FormLabel>
               <FormControl>
                 <Input
-                  className='no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 min-h-[56px] border'
                   placeholder='Where do you live?'
                   {...field}
                 />
@@ -175,7 +175,7 @@ const Profile = ({ clerkId, user }: Params) => {
               <FormControl>
                 <Textarea
                   rows={5}
-                  className='no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px] border'
+                  className='no-focus paragraph-regular light-border-2 background-light800_dark300 text-dark300_light700 min-h-[56px] border'
                   placeholder="What's special about you?"
                   {...field}
                 />
